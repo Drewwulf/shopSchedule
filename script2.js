9ec1179b7bc1a14feea44d3a628cda705e4f627d
Maksim05Cobtah <cobtahmitaap05@gmail.com>
2025-07-26 14:30:30 +0000

    почти до працював

--- script2.js
+++ script2.js
@@ -7,12 +7,54 @@ function y()
  let pp = document.getElementById("op");
  for(let u = 0; u < 20; u += 1)
      {
+
         let ii = document.createElement("td");
+        ii.style.transform = "rotate(-90deg)";
          pp.appendChild(ii);
          im.setDate(im.getDate()+1);
-
+         if(u != 0)
+          {
          ii.textContent =im.toISOString().split("T")[0];
+          }
 
 
      }
+     for(let p  = 1; p < 21; p += 1)
+      {
+       let oof = document.getElementById("yeah");
+       let ppp = document.createElement("td");
+       oof.appendChild(ppp);
+       if(p === 1)
+        {
+         ppp.textContent = "Квіточка";
+        }
+
+      }
+
+       for(let ff = 1; ff < 21; ff += 1)
+      {
+       let back = document.getElementById("hope");
+       let hh = document.createElement("td");
+       back.appendChild(hh);
+       if(ff === 1)
+        {
+         hh.textContent = "Таня";
+        }
+
+      }
+
+      for(let kk  = 1; kk < 21; kk += 1)
+      {
+       let bony = document.getElementById("mimic");
+       let pingwin = document.createElement("td");
+       bony.appendChild(pingwin);
+       if(kk === 1)
+        {
+         pingwin.textContent = "Мирося";
+        }
+
+      }
+
+
+
 }
